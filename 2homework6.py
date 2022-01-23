e1162ffeeee6a75ea38a48129694f8d3efdfaff5
KirillOplachkin <98100933+KirillOplachkin@users.noreply.github.com>
2022-01-23 19:04:43 +0600

    Add files via upload

---
+++ 2homework6.py
@@ -0,0 +1,29 @@
+numbers = int(input('Введите желаемое целое число: '))
+
+
+class Summ:
+    def __init__(self, numbers: list, desired_sum: int):
+        self.numbers = numbers
+        self.desired_sum = desired_sum
+
+    def index(self):
+        for i in range(len(self.numbers)):
+            for n in range(i + 1, len(self.numbers)):
+                if (self.numbers[i] + self.numbers[n]) == self.desired_sum:
+                    return (i, n)
+
+
+num_1 = Summ(numbers=[2, 7, 11, 15], desired_sum=numbers)
+print(numbers)
+print(num_1.index())
+
+
+
+
+
+
+
+
+
+
+
